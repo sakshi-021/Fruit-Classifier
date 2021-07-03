ad481e4cb1abdf6f8b43fab6ddd4ad8240df980a
sakshi-021 <sakshiwp21@gmail.com>
2021-07-03 09:39:45 +0530

    updating

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
 import os
-# from werkzeug.serving import run_simple
-# from flask_ngrok import run_with_ngrok
+from uuid import uuid4
 
 from flask import Flask, request, render_template, send_from_directory
 
-app = Flask(__name__,template_folder="./Templates",static_folder="./static")
-app.debug=True
+app = Flask(__name__)
+# app = Flask(__name__, static_folder="images")
 
 
-APP_ROOT = os.path.dirname(os.getcwd())#return current working directory
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 classes = ['Fresh Apple','Fresh Banana','Fresh Orange','Rotten Apple','Rotten Banana','Rotten Orange']
 
 @app.route("/")
 def index():
-    # return render_template("home.html")
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/upload", methods=["POST"])
 def upload():
-
-
-    target = os.path.join(APP_ROOT, '/images/')
-    
+    target = os.path.join(APP_ROOT, 'images/')
+    # target = os.path.join(APP_ROOT, 'static/')
     print(target)
     if not os.path.isdir(target):
-        os.mkdir(target)
+            os.mkdir(target)
     else:
         print("Couldn't create upload directory: {}".format(target))
     print(request.files.getlist("file"))
@@ -37,35 +34,30 @@ def upload():
         print ("Accept incoming file:", filename)
         print ("Save it to:", destination)
         upload.save(destination)
-        import tensorflow as tf
+        #import tensorflow as tf
         import numpy as np
         from keras.preprocessing import image
 
         from keras.models import load_model
         new_model = load_model('2_model.h5')
         new_model.summary()
-        test_image = image.load_img('/images/'+filename,target_size=(64,64))
+        test_image = image.load_img('images\\'+filename,target_size=(64,64))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = new_model.predict(test_image)
         result1 = result[0]
-        flag=0;
         for i in range(6):
     
-            if result1[i] == 1:
-              flag=1;
-              break;
-        if flag==1:
-          prediction = classes[i]
-          return render_template("template.html",image_name=filename, text=prediction)
-        else:
-          return render_template("template.html",image_name=filename, text="invalid image")
+            if result1[i] == 1.:
+                break;
+        prediction = classes[i]
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("template.html",image_name=filename, text=prediction)
+
 @app.route('/upload/<filename>')
 def send_image(filename):
-    return send_from_directory("/images", filename)
-
-
-
+    return send_from_directory("images", filename)
 
 if __name__ == "__main__":
-     app.run(debug=True,use_reloader=False)
+    app.run(debug=False)
